70bb1b18dd7d36c39deb65cb5bd798bfd71aedd5
Shunxin <lushunxin@hotmail.com>
2016-09-23 17:08:39 -0700

    Improved watermark test.

--- library/src/test/java/org/apache/apex/malhar/lib/window/impl/WindowedJoinOperatorWatermarkTest.java
+++ library/src/test/java/org/apache/apex/malhar/lib/window/impl/WindowedJoinOperatorWatermarkTest.java
@@ -21,9 +21,10 @@ package org.apache.apex.malhar.lib.window.impl;
 import org.junit.Assert;
 import org.junit.Test;
 import org.apache.apex.malhar.lib.window.WindowState;
+import com.datatorrent.lib.testbench.CollectorTestSink;
 
 /**
- *
+ *  Test of windowed join operator handling watermarks.
  */
 public class WindowedJoinOperatorWatermarkTest
 {
@@ -32,19 +33,40 @@ public class WindowedJoinOperatorWatermarkTest
   {
     WindowedJoinOperatorImpl op = new WindowedJoinOperatorImpl();
     op.setWindowStateStorage(new InMemoryWindowedStorage<WindowState>());
+    CollectorTestSink<WatermarkImpl> sink = new CollectorTestSink<>();
+    op.controlOutput.setSink(sink);
+
+    // No watermark is generated if the join operator haven't seen all watermarks from all input streams.
     op.controlInput.process(new WatermarkImpl(1000000));
     op.endWindow();
     Assert.assertEquals(-1, op.currentWatermark);
+    Assert.assertEquals(0, sink.collectedTuples.size());
+
+    // Once both input streams sent watermarks to join operator, it should generate a watermark and send to downstream.
     op.controlInput2.process(new WatermarkImpl(200000));
     op.endWindow();
     Assert.assertEquals(200000, op.currentWatermark);
+    Assert.assertEquals(1, sink.collectedTuples.size());
+
+    // If the minimum of the latest input watermarks changes, join operator should also generate a new watermark.
     op.controlInput2.process(new WatermarkImpl(2100000));
     op.endWindow();
     Assert.assertEquals(1000000, op.currentWatermark);
+    Assert.assertEquals(2, sink.collectedTuples.size());
+
+    // Current watermark of join operator could only change during endWindow() event.
     op.controlInput.process(new WatermarkImpl(1100000));
     Assert.assertEquals(1000000, op.currentWatermark);
     op.endWindow();
     Assert.assertEquals(1100000, op.currentWatermark);
+    Assert.assertEquals(3, sink.collectedTuples.size());
+
+    // If the upstreams sent a watermark but the minimum of the latest input watermarks doesn't change, the join
+    // operator should not generate a new watermark, thus nothing will be sent to downstream.
+    op.controlInput.process(new WatermarkImpl(1100000));
+    op.endWindow();
+    Assert.assertEquals(1100000, op.currentWatermark);
+    Assert.assertEquals(3, sink.collectedTuples.size());
   }
 
 }
